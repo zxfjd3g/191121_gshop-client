e57baa57da86d86e16ced59d33bbd13a55107d2b
zxfjd3g <258147149@qq.com>
2020-04-13 07:32:35 +0800

    imageList

--- src/pages/Detail/ImageList/ImageList.vue
+++ src/pages/Detail/ImageList/ImageList.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="swiper-container">
+  <div class="swiper-container" ref="swiper">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">
-        <img src="../images/s1.png">
-        <img src="../images/s2.png">
-        <img src="../images/s3.png">
-        <img src="../images/s1.png">
-        <img src="../images/s2.png">
-        <img src="../images/s3.png">
-        <img src="../images/s1.png">
-        <img src="../images/s2.png">
-        <img src="../images/s3.png">
+      <div class="swiper-slide" v-for="(skuImg, index) in skuImageList" :key="skuImg.id">
+        <img :src="skuImg.imgUrl" :class="{active: currentIndex===index}">
       </div>
     </div>
     <div class="swiper-button-next"></div>
@@ -19,13 +11,43 @@
 </template>
 
 <script>
-
+  import { mapGetters } from 'vuex'
   import Swiper from 'swiper'
   export default {
     name: "ImageList",
+
+    data () {
+      return {
+        currentIndex: 0 // 当前图片的下标
+      }
+    },
+
+    computed: {
+      ...mapGetters(['skuImageList'])
+    },
+
+    watch: {
+      skuImageList: {
+        handler (value) {
+          // 如果图片数组长度为0, 直接结束
+          if (value.length==0) return
+          // 延迟到界面更新后才创建swiper对象
+          this.$nextTick(() => {
+            this.swiper = new Swiper(this.$refs.swiper, {
+              slidesPerView: 5, // 一次显示5页
+              slidesPerGroup: 2, // 以2页为单位翻页
+              navigation: { //指定翻页按钮
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },
+            })
+          }) 
+        },
+        immediate: true, // 初始化立即调用
+      }
+    },
   }
 </script>
-
 <style lang="less" scoped>
   .swiper-container {
     height: 56px;
