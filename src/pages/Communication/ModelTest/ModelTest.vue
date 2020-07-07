d9e66fb66d9f9a96a64d229be9d07e8c53d1f384
zxfjd3g <258147149@qq.com>
2020-07-07 10:12:13 +0800

    3--v-model

--- src/pages/Communication/ModelTest/ModelTest.vue
+++ src/pages/Communication/ModelTest/ModelTest.vue
@@ -2,9 +2,27 @@
   <div>
     <h2>深入v-model</h2>
 
-    <input type="text">
-    <span>xxx</span>
+    <!-- 
+      原生input上的v-model的本质:  动态的value属性与原生input事件监听
+    -->
+    <input type="text" v-model="msg1">
+    <span>{{msg1}}</span>
     <br>
+    <input type="text" :value="msg2" @input="msg2=$event.target.value">
+    <span>{{msg2}}</span>
+    <br>
+    <br>
+
+    <!-- 
+      组件标签上的v-model的本质:  动态的value属性与自定义input事件监听(将接收的数据更新父组件数据)
+      作用: 实现父子组件间相互通信(数据同步)
+    -->
+    <CustomInput v-model="msg3"/>
+    <span>{{msg3}}</span>
+    <br>
+    <br>
+    <CustomInput :value="msg4" @input="msg4=$event"/>
+    <span>{{msg4}}</span>
   </div>
 </template>
 
@@ -12,6 +30,16 @@
   import CustomInput from './CustomInput.vue'
   export default {
     name: 'ModelTest',
+
+    data () {
+      return {
+        msg1: 'atgugu',
+        msg2: 'atguigu2',
+        msg3: 'atguigu3',
+        msg4: 'atguigu4',
+      }
+    },
+
     components: {
       CustomInput
     }
